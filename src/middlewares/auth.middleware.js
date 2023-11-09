3941db5ea81011d77cc32c401af50475842ebb56
Marian <mcatalin17@gmai.com>
2023-11-09 11:46:30 +0100

    add middelwer for ogenizer and admin

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -24,3 +24,63 @@ export async function validateToken(req, res, next) {
         res.sendStatus(401);
     }
 }
+
+export async function validateAdmin(req, res, next) {
+    // Extrahiere Token aus Cookie
+    const token = req.cookies.access_token
+
+    try {
+        // Versuche gelieferten Token zu verifizieren
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Speichere die aufgelöste Token-Payload im Requestobjekt für weitere Nutzung im Controller
+        req.tokenPayload = payload;
+
+        // Prüfe ob der Nutzer ein Admin ist
+        if (payload.role !== "admin") {
+            // Brich Request ab und sende entsprechenden Fehler zurück
+            res.sendStatus(401);
+        }
+
+        // Schalte Requestverarbeitungskette weiter
+        next();
+
+    } catch (error) {
+        // Nur für debug-Zwecke
+        console.error(error);
+
+        // Token ist ungültig oder abgelaufen
+        // Brich Request ab und sende entsprechenden Fehler zurück
+        res.sendStatus(401);
+    }
+}
+
+export async function validateOrganizerAndAdmin(req, res, next) {
+    // Extrahiere Token aus Cookie
+    const token = req.cookies.access_token
+
+    try {
+        // Versuche gelieferten Token zu verifizieren
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Speichere die aufgelöste Token-Payload im Requestobjekt für weitere Nutzung im Controller
+        req.tokenPayload = payload;
+
+        // Prüfe ob der Nutzer ein Organizer ist
+        if (payload.role !== "organizer" && payload.role !== "admin") {
+            // Brich Request ab und sende entsprechenden Fehler zurück
+            res.sendStatus(401);
+        }
+
+        // Schalte Requestverarbeitungskette weiter
+        next();
+
+    } catch (error) {
+        // Nur für debug-Zwecke
+        console.error(error);
+
+        // Token ist ungültig oder abgelaufen
+        // Brich Request ab und sende entsprechenden Fehler zurück
+        res.sendStatus(401);
+    }
+}
