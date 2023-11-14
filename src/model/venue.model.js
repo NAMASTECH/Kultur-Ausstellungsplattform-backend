f1c7184ebadd21dc7a1a7c99e839ab525f3545da
Andreas Ohligschläger <andreas_ohl_t@hotmail.com>
2023-11-14 16:36:39 +0100

    refactoring

--- src/model/venue.model.js
+++ src/model/venue.model.js
@@ -3,45 +3,47 @@ import { Schema, model } from "mongoose";
 
 const venueSchema = new Schema(
 	{
-		"venueName": {
-		    type: String,
-		    required: true,
-		    cast: false, 
-		 },                       
-		 "venueType": {
-		    type: String,
-		    required: true,
-		    cast: false, 
-		 },
-		 "city": {
-		    type: String,
-		    required: true,
-		    cast: false, 
-		 },
-		 "street": {
-		    type: String,
-		    required: true,
-		    cast: false, 
-		 },
-		 "houseNumber": {
-		    type: String,
-		    required: true,
-		    cast: false, 
-		 },
-		 // Zusatzinfos
-		 "additionalAddressInfo": {
-		    type: String,
-		    cast: false, 
-		 }, 
+		venueName: {
+			type: String,
+			required: true,
+			cast: false,
+		},
+		venueType: {
+			type: String,
+			required: true,
+			cast: false,
+		},
+		city: {
+			type: String,
+			required: true,
+			cast: false,
+		},
+		street: {
+			type: String,
+			required: true,
+			cast: false,
+		},
+		houseNumber: {
+			type: String,
+			required: true,
+			cast: false,
+		},
 		// Postleitzahl        
-		 "ZIP-Code": {
-		    type: String,
-		    required: true,
-		    cast: false, 
-		 }, 
-	}, 
-	{timestamps: true, versionKey: false}
+		zipCode: {
+			type: String,
+			required: true,
+			cast: false,
+		},
+		// Zusatzinfos
+		additionalAddressInfo: {
+			type: String,
+			cast: false,
+		},
+	},
+	{ timestamps: true, versionKey: false }
 );
+
+console.log("Hallo from Venue Model")
 
 // Erstelle Model-Instanz für Venue-Collection
 const Venue = model("venue", venueSchema);
