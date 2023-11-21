0389ee16b702726b9991cdee23e6f6c12b9bf248
Dresca Marian-Catalin <mcatalin17@gmai.com>
2023-11-21 14:53:06 +0100

    Update event controller and artist model

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -152,7 +152,7 @@ export const deleteEvent = async (req, res) => {
 // Abfrage aller Events
 export const getEvents = async (req, res) => {
   try {
-    const events = await Event.find();
+    const events = await Event.find().populate("venues").populate("artists");
     res.json(events);
   } catch (error) {
     res.status(500).json({ message: "Fehler beim Abrufen der Events", error });
@@ -164,7 +164,7 @@ export const getEventById = async (req, res) => {
   try {
     const event = await Event.findById(req.params.id);
     if (!event) {
-      return res.status(404).json({ message: "Event nicht gefunden" });
+      return res.status(404).json({ message: "Event nicht gefunden" }).populate("venues").populate("artists");
     }
     res.json(event);
   } catch (error) {
